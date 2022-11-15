7d89d02d598364c36c84e9007a1b33d068b935d5
Diego Brito <diegorbrito9@gmail.com>
2022-11-15 12:31:11 -0300

    [back end] feat: mudando o balance inicial para 100 reais

--- backend/src/entities/Account/model/account.model.ts
+++ backend/src/entities/Account/model/account.model.ts
@@ -3,7 +3,7 @@ import prisma from "../../../shared/database";
 class AccountModel {
   public async create() {
     const accountCreated = await prisma.accounts.create({
-      data: { balance: 0 },
+      data: { balance: 10000 },
     });
 
     return accountCreated;
